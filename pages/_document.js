f74a07819515ca658692960bd4ed72e6114a7eb7
tvergho <tvergho@gmail.com>
2020-09-25 09:19:07 -0700

    Linkedin image

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+          <meta property="og:image" content="/link.png" />
         </Head>
         <body>
           <Main />
