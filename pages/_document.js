483125f078e315ddaa8c487664c35becd5b4d797
Tyler Vergho <tvergho@gmail.com>
2021-10-26 20:58:20 -0400

    feat: added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-danger */
 import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document';
@@ -15,6 +16,17 @@ class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
           <meta property="og:image" content="/link.png" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-23G7DFNZHN" />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-23G7DFNZHN');
+          `,
+          }}
+          />
         </Head>
         <body>
           <Main />
